f2011cc8e45f752b1a22d05c9d568608a82fb39e
Senific <liveheartstudio@gmail.com>
2021-10-27 19:15:46 +0530

    Create Range class to mimic C# 8 Range feature

--- Index.cs
+++ Index.cs
@@ -1,11 +1,11 @@
-
-using System; 
+using System;
 
 public readonly struct Index
 {
     readonly int value;
-    readonly bool fromEnd; 
-    public Index(int value, bool fromEnd)
+    readonly bool fromEnd;
+
+    public Index( int value, bool fromEnd )
     {
         this.value = value;
         this.fromEnd = fromEnd;
@@ -16,7 +16,7 @@ public readonly struct Index
         return fromEnd ? length - value : value;
     }
 
-    public int GetIndex(Array array)
+    public int GetIndex( Array array )
     {
         return GetIndex( array.Length );
     }
---
+++ Range.cs
@@ -0,0 +1,19 @@
+using System;
+
+public readonly struct Range
+{
+    public Range( Index start, Index end )
+    {
+        throw new NotImplementedException();
+    }
+
+    public static Range StartAt( Index start )
+    {
+        throw new NotImplementedException(); }
+
+    public static Range EndAt( Index end )
+    {
+        throw new NotImplementedException();
+    }
+    public static Range All { get; }
+}
