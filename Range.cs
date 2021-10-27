e8fbb0cefd8b65ccce49d8bc78be1c5308d9b938
Senific <liveheartstudio@gmail.com>
2021-10-27 19:35:14 +0530

    Add a comment to highlight Inclusive/Exclusive state of params

--- Range.cs
+++ Range.cs
@@ -5,6 +5,11 @@ public readonly struct Range
     readonly Index start;
     readonly Index end;
     
+    /// <summary>
+    /// Filters a range of a collection
+    /// </summary>
+    /// <param name="start">inclusive</param>
+    /// <param name="end">exclusive</param>
     public Range( Index start, Index end )
     {
         this.start = start;
